6c697f7d4253ab66903f2d1573ba7c6d4575fb3c
drofford <56809194+drofford@users.noreply.github.com>
2023-08-09 20:32:43 -0500

    Adding a utils module

--- 02-quicksort/src/main.rs
+++ 02-quicksort/src/main.rs
@@ -1,3 +1,31 @@
+mod partition;
+mod utils;
+
+use partition::{
+    partition,
+};
+
+use utils::{
+    print_vec,
+};
+
 fn main() {
     println!("Hello, world!");
+    let r = partition();
+    println!("result = {}", r);
+
+    vector_play();
 }
+
+const SIZE: i32 = 10;
+
+fn vector_play() {
+    let mut vec: Vec<i32> = Vec::with_capacity(SIZE as usize);
+
+    for i in 0..SIZE {
+        vec.push(i);
+    }
+
+    print_vec(&vec, 2 * SIZE);
+}
+
---
+++ 02-quicksort/src/partition.rs
@@ -0,0 +1,32 @@
+// partition.rs
+
+/* 
+ The Lomuto algorithm from Wikipedia
+
+// Divides array into two partitions
+algorithm partition(A, lo, hi) is 
+  pivot := A[hi] // Choose the last element as the pivot
+
+  // Temporary pivot index
+  i := lo - 1
+
+  for j := lo to hi - 1 do 
+    // If the current element is less than or equal to the pivot
+    if A[j] <= pivot then 
+      // Move the temporary pivot index forward
+      i := i + 1
+      // Swap the current element with the element at the temporary pivot index
+      swap A[i] with A[j]
+
+  // Move the pivot element to the correct pivot position (between the smaller and larger elements)
+  i := i + 1
+  swap A[i] with A[hi]
+  return i // the pivot index
+ 
+*/
+
+
+
+pub fn partition() -> i32 {
+    return 4;
+}
