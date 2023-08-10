ac3e7de71a5526954c9f9d4c6491c43dc94372f3
drofford <56809194+drofford@users.noreply.github.com>
2023-08-09 20:39:33 -0500

    WIP

--- 02-quicksort/src/main.rs
+++ 02-quicksort/src/main.rs
@@ -6,26 +6,18 @@ use partition::{
 };
 
 use utils::{
+    make_random_vec,
     print_vec,
 };
+
+const SIZE: i32 = 100;
 
 fn main() {
     println!("Hello, world!");
     let r = partition();
     println!("result = {}", r);
 
-    vector_play();
+    let mut values = make_random_vec(SIZE, 10);
+
+    print_vec(&values, SIZE)
 }
-
-const SIZE: i32 = 10;
-
-fn vector_play() {
-    let mut vec: Vec<i32> = Vec::with_capacity(SIZE as usize);
-
-    for i in 0..SIZE {
-        vec.push(i);
-    }
-
-    print_vec(&vec, 2 * SIZE);
-}
-
